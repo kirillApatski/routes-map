01be0e831cfc876f9b5739a31cd369db4ffa12af
Kirill <apatski94@gmail.com>
2023-07-26 00:11:31 +0300

    feat: fix styles and logic

--- src/components/maps/map.tsx
+++ src/components/maps/map.tsx
@@ -1,36 +1,30 @@
-import { MapContainer, TileLayer, Polyline, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet'
 
+import { mapZoom, polylineWeight } from '../../common/constants/constants.ts'
+import { calculateCenter } from '../../common/helpers/centre-route.ts'
 import { reverceCoordinate } from '../../common/helpers/reverce-coordinate.ts'
 import { useAppSelector } from '../../common/hooks/redux-hooks.ts'
 import { getPolylineCoordinates } from '../../redux/selectors/polyline-selectors.ts'
 import { getRouteRoad } from '../../redux/selectors/routes-road-selectors.ts'
 
-const A = () => {
-  const map = useMap()
+import { CenterRoute } from './centre-route.tsx'
+import s from './map.module.scss'
 
-  console.log('map center:', map)
-
-  return null
-}
-
-export const Maps = () => {
+export const Map = () => {
   const coordinates = useAppSelector(getPolylineCoordinates)
   const route = useAppSelector(getRouteRoad)
 
   const position = reverceCoordinate(coordinates)
+  const centerRoute = calculateCenter(coordinates)
 
   return (
-    <MapContainer
-      style={{ overflow: 'hidden', width: '100%', height: 'calc(100vh - 129px)' }}
-      center={route[0].dots[0]}
-      zoom={13}
-    >
-      <A />
+    <MapContainer className={s.container} center={centerRoute} zoom={mapZoom}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Polyline weight={5} positions={position} />
+      <CenterRoute coordinates={centerRoute} />
+      <Polyline weight={polylineWeight} positions={position} />
       {position.length &&
         route[0].dots.map((point: [number, number], index: number) => {
           return (
