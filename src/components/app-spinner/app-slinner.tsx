ce21ab3260fc5c72efc40236956d533c703fe23c
Kirill <apatski94@gmail.com>
2023-07-26 00:16:43 +0300

    feat: add spinner and styles

---
+++ src/components/app-spinner/app-slinner.tsx
@@ -0,0 +1,12 @@
+import { Space, Spin } from 'antd'
+
+import s from './app-spinner.module.scss'
+
+type SpinnerPropsType = {
+  spinning: boolean
+}
+export const Spinner: React.FC<SpinnerPropsType> = ({ spinning }) => (
+  <Space size="large" className={s.container}>
+    <Spin spinning={spinning} tip="Loading" size="large" />
+  </Space>
+)
