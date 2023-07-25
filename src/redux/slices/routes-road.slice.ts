074e87c51a85c6abbc957fe7d441fefd9f8f7bc0
Kirill <apatski94@gmail.com>
2023-07-26 00:44:00 +0300

    test: test routes

--- src/redux/slices/routes-road.slice.ts
+++ src/redux/slices/routes-road.slice.ts
@@ -42,6 +42,28 @@ const initialState: RoutesDataType[] = [
     ],
     isSelected: false,
   },
+  {
+    key: '4',
+    name: 'Маршрут Минск 1',
+    dotsName: 'dotsFour',
+    dots: [
+      [54.002627, 27.570563],
+      [53.908449, 27.547963],
+      [53.854809, 27.608903],
+    ],
+    isSelected: false,
+  },
+  {
+    key: '5',
+    name: 'Маршрут Минск 2',
+    dotsName: 'dotsFive',
+    dots: [
+      [53.854809, 27.608903],
+      [53.865499, 27.485564],
+      [53.908662, 27.431607],
+    ],
+    isSelected: false,
+  },
 ]
 
 const routesRoadSlice = createSlice({
