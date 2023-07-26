74a454e4b804b3331cdde4476416d5e6c62f9d7a
Kirill <apatski94@gmail.com>
2023-07-26 11:46:51 +0300

    fix: delete StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import './styles/index.scss'
-import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
@@ -10,9 +9,7 @@ import { App } from './App.tsx'
 import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
