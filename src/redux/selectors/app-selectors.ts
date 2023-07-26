49bac0834c25085c44874481f9160971124d5603
Kirill <apatski94@gmail.com>
2023-07-26 12:31:43 +0300

    feat: add selectors

--- src/redux/selectors/app-selectors.ts
+++ src/redux/selectors/app-selectors.ts
@@ -1,3 +1,5 @@
 import { RootState } from '../store.ts'
 
 export const getLoading = (state: RootState): boolean => state.app.loading
+export const getErrorMessages = (state: RootState): string | null => state.app.error
+export const getErrorCode = (state: RootState): string | null | undefined => state.app.code
