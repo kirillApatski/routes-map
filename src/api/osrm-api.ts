0c261c536d9fbd389dc2556311675ca08639e057
Kirill <apatski94@gmail.com>
2023-07-26 12:29:32 +0300

    feat: add types

--- src/api/osrm-api.ts
+++ src/api/osrm-api.ts
@@ -1,7 +1,25 @@
 import { instance } from './instance/instance.ts'
 
 export const osrmApi = {
-  getOsrmRoutes(routePoints: number[]): any {
-    return instance.get<any>(`${routePoints}?overview=full&geometries=geojson`)
+  getOsrmRoutes(routePoints: number[]) {
+    return instance.get<ResponseType>(`${routePoints}?overview=full&geometries=geojson`)
   },
+}
+
+type ResponseType = {
+  code: string
+  routes: [
+    {
+      geometry: {
+        coordinates: [number, number][]
+        waypoints: WaypointsType[]
+      }
+    },
+  ]
+}
+type WaypointsType = {
+  hint: string
+  distance: number
+  name: string
+  location: [number, number]
 }
