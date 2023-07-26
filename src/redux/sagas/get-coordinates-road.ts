69e258489d5b3387d231414744894a7fcda865be
Kirill <apatski94@gmail.com>
2023-07-26 12:30:00 +0300

    feat: add setAppError

--- src/redux/sagas/get-coordinates-road.ts
+++ src/redux/sagas/get-coordinates-road.ts
@@ -1,22 +1,23 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
 
 import { osrmApi } from '../../api/osrm-api.ts'
-import { getCoordinates } from '../actions/get-coordinates.ts'
-import { setAppLoading } from '../slices/app.slice.ts'
+import { getCoordinates, GetCoordinatesType } from '../actions/get-coordinates.ts'
+import { setAppError, setAppLoading } from '../slices/app.slice.ts'
 import { setСoordinates } from '../slices/coordinates.slice.ts'
 
 export function* coordinatesWatcher() {
   yield takeEvery(getCoordinates.type, getСoordinatesRoad)
 }
 
-export function* getСoordinatesRoad(actions: any): any {
+export function* getСoordinatesRoad(actions: GetCoordinatesType): any {
   yield put(setAppLoading({ loading: true }))
   try {
     const res = yield call(osrmApi.getOsrmRoutes, actions.payload.params)
 
     yield put(setСoordinates(res.data.routes[0].geometry.coordinates))
     yield put(setAppLoading({ loading: false }))
-  } catch (error) {
-    // console.log(error)
+  } catch (error: any) {
+    yield put(setAppError({ error: error.response.data.message, code: error.response.data.code }))
+    yield put(setAppLoading({ loading: false }))
   }
 }
