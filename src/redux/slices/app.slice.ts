575cc2b539e6e0e416d84eb11c0352888f431042
Kirill <apatski94@gmail.com>
2023-07-26 11:45:39 +0300

    test: add setAppError

--- src/redux/slices/app.slice.ts
+++ src/redux/slices/app.slice.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 export type InitialStateType = {
   loading: boolean
   error: string | null
+  code?: string | null
 }
 
 const initialState: InitialStateType = {
   loading: false,
   error: null,
+  code: null,
 }
 
 const appSlice = createSlice({
@@ -17,8 +19,12 @@ const appSlice = createSlice({
     setAppLoading(state, action: PayloadAction<{ loading: boolean }>) {
       state.loading = action.payload.loading
     },
+    setAppError(state, action: PayloadAction<{ error: string | null; code: string | null }>) {
+      state.error = action.payload.error
+      state.code = action.payload.code
+    },
   },
 })
 
-export const { setAppLoading } = appSlice.actions
+export const { setAppLoading, setAppError } = appSlice.actions
 export const appReducer = appSlice.reducer
